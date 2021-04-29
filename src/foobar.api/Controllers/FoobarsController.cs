ce0ffffc94b48a63ae1cd955b6023b416ab4e2fe
Alex Wolek <alex.wolek@gmail.com>
2021-04-28 22:43:41 -0500

    adding dot net web api project

---
+++ src/foobar.api/Controllers/FoobarsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Foobar.Api.Controllers
+{
+    [ApiController]
+    [Route("foobars")]
+    public class FoobarsController : ControllerBase
+    {
+        private readonly ILogger<FoobarsController> _logger;
+
+        public FoobarsController(ILogger<FoobarsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var foobars = new [] 
+            {
+                new {foo = "bar"},
+                new {foo = "baz"},
+            };
+
+            return Ok(foobars);
+        }
+    }
+}
